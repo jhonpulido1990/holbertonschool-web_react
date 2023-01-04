0caadbe74d5d724695e91588856568da444b3a81
jhonpulido1990 <3825@holbertonstudents.com>
2023-01-04 16:00:02 -0500

    add new file task 2

--- 0x01-Webpack/task_2/js/dashboard_main.js
+++ 0x01-Webpack/task_2/js/dashboard_main.js
@@ -2,12 +2,14 @@ import $ from 'jquery'
 import _ from 'lodash'
 
 // add element in html
+$('head').append('<link rel="stylesheet" href="../css/main.css" type="text/css" />');
 $('body').append('<p>Holberton Dashboard</p>');
 $('body').append('<p>Dashboard data for the students</p>');
 $('body').append('<button>Click here to get started</button>');
 $('body').append("<p id='count'></p>");
 $('body').append('<p>Copyright - Holberton School</p>');
 $('button').on('click', _.debounce(updateCounter));
+$('body').append("<img id='logo'>")
 
 let count = 0;
 
--- 0x01-Webpack/task_2/webpack.config.js
+++ 0x01-Webpack/task_2/webpack.config.js
@@ -1,10 +1,34 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
-  entry: './js/dashboard_main.js',
-  mode: 'production',
+  mode: "production",
+  entry: "./js/dashboard_main.js",
   output: {
-    path: path.resolve(__dirname, 'public'),
-    filename: 'bundle.js'
+    path: path.resolve(__dirname, "public"),
+    filename: "bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        // add css-loader ========
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        // add the file-loader =======
+        test: /\.(gif|png|jpe?g|svg)$/i,
+        use: [
+          "file-loader",
+          {
+            // add image-webpack-loader =====0
+            loader: "image-webpack-loader",
+            options: {
+              bypassOnDebug: true, // webpack@1.x
+              disable: true, // webpack@2.x and newer
+            },
+          },
+        ],
+      },
+    ],
   },
 };
