39b1280bc82be6fb44c7988afa9d5cfadb1fc1d9
jhonpulido1990 <3825@holbertonstudents.com>
2023-01-05 11:26:31 -0500

    change dashboard

--- 0x01-Webpack/task_1/js/dashboard_main.js
+++ 0x01-Webpack/task_1/js/dashboard_main.js
@@ -1,17 +1,17 @@
-import $ from 'jquery'
-import _ from 'lodash'
+import $ from 'jquery';
+import _ from 'lodash';
 
-// add element in html
 $('body').append('<p>Holberton Dashboard</p>');
 $('body').append('<p>Dashboard data for the students</p>');
 $('body').append('<button>Click here to get started</button>');
-$('body').append("<p id='count'></p>");
+$('body').append('<p id="count"></p>');
 $('body').append('<p>Copyright - Holberton School</p>');
-$('button').on('click', _.debounce(updateCounter));
 
 let count = 0;
 
-function updateCounter(){
-  count += 1;
-  $('#count').text(`${count.toString()} clicks on the button`)
-}
+export const updateCounter = () => {
+  count++;
+  $('#count').text(`${count} clicks on the button`);
+};
+
+$('button').on('click', _.debounce(updateCounter, 500));
