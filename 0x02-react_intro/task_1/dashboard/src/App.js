8cec6581c7ebd405f6567966cdbd7af08f4d4a69
jhonpulido1990 <3825@holbertonstudents.com>
2023-01-10 15:42:26 -0500

    task 2

--- 0x02-react_intro/task_1/dashboard/src/App.js
+++ 0x02-react_intro/task_1/dashboard/src/App.js
@@ -13,6 +13,12 @@ function App() {
       </header>
       <body className='App-body'>
         <p>Login to access the full dashboard</p>
+        {/* https://reactjs.org/docs/dom-elements.html */}
+        <label htmlFor="email">Email: </label>
+        <input type="email" id="email" />
+        <label htmlFor="psw">Password: </label>
+        <input type="password" id="psw" />
+        <button>OK</button>
       </body>
       <footer>
         <p className='App-footer'>Copyright { getFullYear() } - {getFooterCopy(true)}</p>
