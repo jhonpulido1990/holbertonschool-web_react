02c0649529025f0733c67e0552e849b15dc1bdcb
jhonpulido1990 <3825@holbertonstudents.com>
2023-01-09 22:02:20 -0500

    task 0

---
+++ 0x02-react_intro/task_0/dashboard/src/App.js
@@ -0,0 +1,23 @@
+import logo from './define.jpg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>
+          School dashboard
+        </h1>
+      </header>
+      <body className='App-body'>
+        <p>Login to access the full dashboard</p>
+      </body>
+      <footer>
+        <p className='App-footer'>Copyright 2020 - holberton School</p>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
