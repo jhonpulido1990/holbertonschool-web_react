4b2aa1e992d55575d50f6bf586080364a948b9f3
jhonpulido1990 <3825@holbertonstudents.com>
2023-01-25 17:58:00 -0500

    task 1

--- 0x05-React_inline_styling/task_1/dashboard/src/Notifications/Notifications.test.js
+++ 0x05-React_inline_styling/task_1/dashboard/src/Notifications/Notifications.test.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import { expect } from 'chai';
 import { shallow } from 'enzyme';
+import { StyleSheetTestUtils } from 'aphrodite';
 
 import Notifications from './Notifications'
 import NotificationItem from './NotificationItem';
 
-describe('<Notifications />', () => {
+describe('Notifications', () => {
+  beforeEach(() => {
+    StyleSheetTestUtils.suppressStyleInjection();
+  });
+
+  afterEach(() => {
+    StyleSheetTestUtils.clearBufferAndResumeStyleInjection();
+  });
+
   test('renders without crashing', () => {
     const wrapper = shallow(<Notifications />);
 
@@ -20,14 +29,14 @@ describe('<Notifications />', () => {
     });
     test('menu item is displayed', () => {
       const wrapper = shallow(<Notifications displayDrawer={true} />);
-      const menuItem = wrapper.find('.menuItem');
+      const menuItem = wrapper.find('[data-testid="menu-item"]');
 
       expect(menuItem).to.have.lengthOf(1);
     });
 
     test('notifications div is displayed', () => {
       const wrapper = shallow(<Notifications displayDrawer={true} />);
-      const notifs = wrapper.find('div.Notifications');
+      const notifs = wrapper.find('[data-testid="notifs"]');
 
       expect(notifs).to.have.lengthOf(1);
     });
@@ -36,7 +45,7 @@ describe('<Notifications />', () => {
   describe('displayDrawer is false', () => {
     test('menu item is displayed', () => {
       const wrapper = shallow(<Notifications />);
-      const menuItem = wrapper.find('.menuItem');
+      const menuItem = wrapper.find('[data-testid="menu-item"]');
 
       expect(menuItem).to.have.lengthOf(1);
     });
@@ -79,7 +88,7 @@ describe('<Notifications />', () => {
 
   describe('message displays properly', () => {
     const wrapper = shallow(<Notifications displayDrawer={true} />);
-    const noNewNotifs = wrapper.find('.Notifications p');
+    const noNewNotifs = wrapper.find('[data-testid="notifs"] p');
 
     expect(noNewNotifs).to.have.lengthOf(1);
     expect(noNewNotifs.text()).to.equal('No new notifications for now');
