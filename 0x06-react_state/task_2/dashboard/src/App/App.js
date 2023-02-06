3d3f0664208573cf034a4d037695e7c425beda4e
jhonpulido1990 <3825@holbertonstudents.com>
2023-02-06 14:25:01 -0500

    add task 1

---
+++ 0x06-react_state/task_2/dashboard/src/App/App.js
@@ -0,0 +1,126 @@
+import React, { Fragment, Component } from 'react';
+import PropTypes from 'prop-types';
+import { StyleSheet, css } from 'aphrodite';
+
+import Header from '../Header/Header';
+import Footer from '../Footer/Footer';
+import Login from '../Login/Login';
+import Notifications from '../Notifications/Notifications';
+import CourseList from '../CourseList/CourseList';
+import BodySection from '../BodySection/BodySection';
+import BodySectionWithMarginBottom from '../BodySection/BodySectionWithMarginBottom';
+
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.handleLogout = this.handleLogout.bind(this);
+    this.handleDisplayDrawer = this.handleDisplayDrawer.bind(this);
+    this.handleHideDrawer = this.handleHideDrawer.bind(this);
+    this.state = {displayDrawer: false};
+  }
+
+  handleDisplayDrawer() {
+    this.setState({displayDrawer: true});
+}
+
+  handleHideDrawer() {
+    this.setState({displayDrawer: false});
+}
+
+
+  componentDidMount() {
+      window.addEventListener("keydown", this.handleLogout);
+  }
+
+  componentWillUnmount() {
+      window.removeEventListener("keydown", this.handleLogout);
+  }
+
+  handleLogout (event) {
+      if (event.ctrlKey && event.key === 'h') {
+          event.preventDefault();
+          alert("Logging you out");
+          this.props.logOut();
+      }
+    }
+
+  render() {
+    const { isLoggedIn } = this.props;
+
+    const listCourses = [
+      { id: 1, name: 'ES6', credit: 60 },
+      { id: 2, name: 'Webpack', credit: 20 },
+      { id: 3, name: 'React', credit: 40 }
+    ];
+
+    const listNotifications = [
+      { id: 1, type: 'default', value: 'New course available' },
+      { id: 2, type: 'urgent', value: 'New resume available' },
+      {
+        id: 3,
+        type: 'urgent',
+        html: { __html: '<strong>Urgent requirement</strong> - complete by EOD' }
+      }
+    ];
+
+    return (
+      <Fragment>
+        <Notifications
+        listNotifications={listNotifications}
+        displayDrawer={this.state.displayDrawer}
+        handleDisplayDrawer={this.handleDisplayDrawer}
+        handleHideDrawer={this.handleHideDrawer}
+        />
+        <div className={css(styles['sans-serif'])}>
+          <Header />
+            <div className={css(styles.padding)}>
+              {!isLoggedIn && (
+                  <BodySectionWithMarginBottom title='Log in to continue'>
+                    <Login />
+                  </BodySectionWithMarginBottom>
+                )}
+                {isLoggedIn && (
+                  <BodySectionWithMarginBottom title='Course List'>
+                    <CourseList listCourses={listCourses} />
+                  </BodySectionWithMarginBottom>
+                )}
+                <BodySection title='News from the School'>
+                  <p>
+                    A paragraph with some random text. A paragraph with some random text.
+                    A paragraph with some random text. A paragraph with some random text.
+                    A paragraph with some random text. A paragraph with some random text.
+                  </p>
+                </BodySection>
+              </div>
+          <Footer />
+        </div>
+      </Fragment>
+    );
+  }
+}
+
+App.propTypes = {
+  isLoggedIn: PropTypes.bool,
+  logOut: PropTypes.func
+};
+
+App.defaultProps = {
+  isLoggedIn: false,
+  logOut: () => undefined
+};
+
+const styles = StyleSheet.create({
+  'sans-serif': {
+    fontFamily: 'sans-serif'
+  },
+  padding: {
+    '@media (min-width: 901px)': {
+      padding: '4rem'
+    },
+    '@media (max-width: 900px)': {
+      padding: '2rem 2rem 50px 2rem'
+    }
+  }
+});
+
+export default App;
---
+++ 0x06-react_state/task_2/dashboard/src/Notifications/Notifications.test.js
@@ -0,0 +1,123 @@
+import React from 'react';
+import { expect as expect2 } from 'chai';
+import { shallow } from 'enzyme';
+import { StyleSheetTestUtils } from 'aphrodite';
+
+import Notifications from './Notifications'
+import NotificationItem from './NotificationItem';
+
+describe('Notifications', () => {
+  beforeEach(() => {
+    StyleSheetTestUtils.suppressStyleInjection();
+  });
+
+  afterEach(() => {
+    StyleSheetTestUtils.clearBufferAndResumeStyleInjection();
+  });
+
+  test('renders without crashing', () => {
+    const wrapper = shallow(<Notifications />);
+
+    expect2(wrapper.exists());
+  });
+
+  describe('displayDrawer is true', () => {
+    test('has a close button', () => {
+      const wrapper = shallow(<Notifications displayDrawer={true} />);
+
+      expect2(wrapper.find('img')).to.have.lengthOf(1);
+    });
+    test('menu item is displayed', () => {
+      const wrapper = shallow(<Notifications displayDrawer={true} />);
+      const menuItem = wrapper.find('[data-testid="menu-item"]');
+
+      expect2(menuItem).to.have.lengthOf(1);
+    });
+
+    test('notifications div is displayed', () => {
+      const wrapper = shallow(<Notifications displayDrawer={true} />);
+      const notifs = wrapper.find('[data-testid="notifs"]');
+
+      expect2(notifs).to.have.lengthOf(1);
+    });
+  });
+
+  describe('displayDrawer is false', () => {
+    test('menu item is displayed', () => {
+      const wrapper = shallow(<Notifications />);
+      const menuItem = wrapper.find('[data-testid="menu-item"]');
+
+      expect2(menuItem).to.have.lengthOf(1);
+    });
+
+    test('notifications div is not displayed', () => {
+      const wrapper = shallow(<Notifications />);
+      const notifs = wrapper.find('div.Notifications');
+
+      expect2(notifs).to.have.lengthOf(0);
+    });
+  });
+
+  describe('listNotifications is empty', () => {
+    test('renders correctly if empty array is passed', () => {
+      const wrapper = shallow(<Notifications />);
+      const notifs = wrapper.find(NotificationItem);
+
+      expect2(notifs).to.have.lengthOf(0);
+    });
+
+    test('renders correctly if listNotifications prop not specified', () => {
+      const wrapper = shallow(<Notifications />);
+      const notifs = wrapper.find(NotificationItem);
+
+      expect2(notifs).to.have.lengthOf(0);
+    });
+  });
+
+  describe('listNotifications is not empty', () => {
+    test('renders a list with two items when passed two items', () => {
+      const nots = [
+        {id: 1, type: "default", value: "x"},
+        {id: 2, type: "default", value: "y"}
+      ];
+      const wrapper = shallow(<Notifications displayDrawer={true} listNotifications={nots} />);
+      expect2(wrapper.find("ul")).to.have.lengthOf(1);
+      expect2(wrapper.find(NotificationItem)).to.have.lengthOf(2);
+    });
+  });
+
+  describe('message displays properly', () => {
+    const wrapper = shallow(<Notifications displayDrawer={true} />);
+    const noNewNotifs = wrapper.find('[data-testid="notifs"] p');
+
+    expect2(noNewNotifs).to.have.lengthOf(1);
+    expect2(noNewNotifs.text()).to.equal('No new notifications for now');
+  });
+  test('clicking menu item calls handleDisplayDrawer', () => {
+    const handleDisplayDrawer = jest.fn();
+
+    const wrapper = shallow(
+      <Notifications handleDisplayDrawer={handleDisplayDrawer} />
+    );
+
+    const menuItem = wrapper.find('[data-testid="menu-item"]');
+    expect2(menuItem).to.have.lengthOf(1);
+
+    menuItem.simulate('click');
+    expect(handleDisplayDrawer).toHaveBeenCalledTimes(1);
+  });
+
+  test('clicking close button calls hideDisplayDrawer', () => {
+    const handleHideDrawer = jest.fn();
+
+    const wrapper = shallow(
+      <Notifications displayDrawer={true} handleHideDrawer={handleHideDrawer} />
+    );
+
+    const close = wrapper.find('[data-testid="close-notifs"]');
+    expect2(close).to.have.lengthOf(1);
+
+    close.simulate('click');
+    expect(handleHideDrawer).toHaveBeenCalledTimes(1);
+  });
+});
